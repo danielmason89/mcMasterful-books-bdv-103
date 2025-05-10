5f28bec641924bf6fbe01323970a0cdc0de5cb2d
Daniel Mason <danmasson0@gmail.com>
2025-05-10 18:33:50 +0000

    implemented sod schema for validating query parameters

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,33 @@
-import Router from 'koa-router';
+import { z } from "zod"
+import createRouter from "koa-zod-router";
 import adapter from '../adapter';
-const router = new Router();
+
+const router = createRouter();
 
 // testing
+// Zod schema for validating query parameters
+const filterSchema = z.object({
+    filters: z
+    .string()
+    .optional()
+    .transform((val) => {
+        try {
+            const parsed = JSON.parse(val ?? "[]");
+            return Array.isArray(parsed) ? parsed : [];
+        } catch {
+            return [];
+        }
+    })
+    .refine((arr) => {
+        arr.every(
+            (filter) =>
+                typeof filter === "object" &&
+            ("from" in filter ? typeof filter.from === "number" : true) &&
+            ("to" in filter ? typeof filter.to === "number" : true)
+        ),
+        { message: "invalid filter" }
+    })
+})
 router.get('/', (ctx) => {
     ctx.body = { message: 'Hello World' };
 });
