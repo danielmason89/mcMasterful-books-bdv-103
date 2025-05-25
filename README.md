e39ecb7b1e08a7d5a2bfbf0937e7b0e82fe4f2ba
Daniel Mason <danmasson0@gmail.com>
2025-05-25 19:40:37 +0000

    completed assignment-3 filtering implementation, and CI/CD implementation

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # bvd-103-assignment-1-template
+![CI](https://github.com/danielmason89/mcMasterful-books-bdv-103/actions/workflows/ci.yml/badge.svg)
 
 ## Install node packages
 ```
--- adapter/assignment-3.ts
+++ adapter/assignment-3.ts
@@ -1,4 +1,5 @@
 import previous_assignment from './assignment-2'
+import BookModel from '../src/models/book'
 
 export type BookID = string
 
@@ -21,7 +22,30 @@ export interface Filter {
 // If multiple filters are provided, any book that matches at least one of them should be returned
 // Within a single filter, a book would need to match all the given conditions
 async function listBooks (filters?: Filter[]): Promise<Book[]> {
-  throw new Error("Todo")
+  if (!filters || filters.length === 0) {
+    const allBooks = await BookModel.find({});
+    return allBooks.map((b) => ({ ...b.toObject(), id: b._id.toString() }));
+  }
+
+  const mongoFilters = filters.map((filter) => {
+    const conditions: Record<string, unknown> = {};
+    if (filter.from !== undefined || filter.to !== undefined) {
+      conditions.price = {
+      ...(filter.from !== undefined ? { $gte: filter.from } : {}),
+      ...(filter.to !== undefined ? { $lte: filter.to } : {})
+    }
+  }
+    if (filter.name) {
+      conditions.name = { $regex: filter.name, $options: 'i' };
+    }
+    if (filter.author) {
+      conditions.author = { $regex: filter.author, $options: 'i' };
+    }
+    return conditions;
+  });
+
+  const books = await BookModel.find({ $or: mongoFilters });
+  return books.map((b) => ({ ...b.toObject(), id: b._id.toString() }));
 }
 
 async function createOrUpdateBook (book: Book): Promise<BookID> {
