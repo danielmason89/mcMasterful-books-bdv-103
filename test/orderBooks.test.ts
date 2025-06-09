619e7111b3eb216057c2451794b8be11f3f5fef1
Daniel Mason <danmasson0@gmail.com>
2025-06-09 06:45:09 +0000

    made changes to tests to get the rest of the test to pass part #8

--- test/orderBooks.test.ts
+++ test/orderBooks.test.ts
@@ -4,6 +4,7 @@ import { it, expect, beforeAll, beforeEach } from 'vitest'
 import OrderModel from '../src/models/order.js'
 import BookModel from '../src/models/book.js'
 import ShelfModel from '../src/models/shelf.js'
+import { mongoWarehouse } from '../src/adapters/mongoWarehouse.js'
 
 import { connectToDatabase } from '../src/lib/db.js'
 
@@ -18,6 +19,7 @@ beforeEach(async () => {
 })
 
 it('creates an order and returns orderId', async () => {
+  // Create a book
   const book = await BookModel.create({
     name: 'Order Test',
     author: 'O',
@@ -26,10 +28,15 @@ it('creates an order and returns orderId', async () => {
     price: 10
   })
 
+  // Add stock for the book using the mongoWarehouse adapter
+  await mongoWarehouse.placeBooksOnShelf(book._id.toString(), 'A1', 2)
+
+  // Create an order for 2 units of the book
   const { orderId } = await assignment4.orderBooks({
     [book._id.toString()]: 2
   })
 
+  // Retrieve the order and check if the order has the correct quantity
   const order = await OrderModel.findOne({ orderId })
 
   expect(order?.books[book._id.toString()]).toBe(2)
