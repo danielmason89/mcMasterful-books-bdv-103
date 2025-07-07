6af4dde7dd678fc1f2f66f0e9253445da30e9045
Daniel Mason <danmasson0@gmail.com>
2025-07-07 01:38:50 +0000

    fix:made changes to ci.yml to fix pipeline part #2

--- test/test.orderApi.spec.ts
+++ test/test.orderApi.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest';
 import { Configuration } from '../client';
-import { DefaultApi } from '../client/apis/DefaultApi';
+import { OrdersApi } from '../client/apis/OrdersApi';
 
 
 describe('API: Order Routes', () => {
   it('should create an order', async () => {
-    const client = new DefaultApi(new Configuration({ basePath: "http://localhost:3000/" }));
+    const client = new OrdersApi(new Configuration({ basePath: "http://localhost:3000/" }));
     const res = await client.createOrder({ requestBody: ['book-id-1', 'book-id-2']});
     expect(res.orderId).toBeDefined();
   });
